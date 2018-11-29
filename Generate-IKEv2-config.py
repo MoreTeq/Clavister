ef431eba9f52dab8984403b74478b0d8f744263a
MoreTeq <roel@moreteq.com>
2018-11-29 11:58:58 +0100

    Adding script

--- Generate-IKEv2-config.py
+++ Generate-IKEv2-config.py
@@ -1,1 +1,13 @@
+'''
+Generate Roaming VPN (Simplified) config for Clavister 12.00.13
+Needed for this script:
+- CA Cert
+- Gateway Cert
+- Gateway Private Key
+
+The generated script will add a variety of objects used for the Roaming VPN configuration.
+
+Author: Roel van den Bussche
+'''
+
 print("More to come...")

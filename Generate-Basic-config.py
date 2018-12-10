4d1d470618924283e1f6f8242e0500f35e9e49bc
MoreTeq <roel@moreteq.com>
2018-12-10 12:05:46 +0100

    Added

--- Generate-Basic-config.py
+++ Generate-Basic-config.py
@@ -61,6 +61,8 @@ for interface in interfaces:
         config_file.write("set Interface Ethernet " + interface_name + " DHCPEnabled=Yes")
         config_file.write("\n")
     elif interface_dhcp == False:
+        config_file.write("set Interface Ethernet " + interface_name + " DHCPEnabled=No")
+        config_file.write("\n")
         if interface_ip:
             config_file.write("set Address IP4Address " + interfaceFolder + "/" + interface_name + "_ip Address=" + interface_ip)
             config_file.write("\n")
@@ -72,10 +74,10 @@ for interface in interfaces:
             config_file.write("\n")
 
         if interface_gw:
-            config_file.write("set Interface Ethernet " + interface_name + " DHCPEnabled=No IP=" + interfaceFolder + "/" + interface_name + "_ip DefaultGateway=" + interfaceFolder + "/" + interface_name + "_gw")
+            config_file.write("set Interface Ethernet " + interface_name + " IP=" + interfaceFolder + "/" + interface_name + "_ip DefaultGateway=" + interfaceFolder + "/" + interface_name + "_gw")
             config_file.write("\n")
         else:
-            config_file.write("set Interface Ethernet " + interface_name + " DHCPEnabled=No IP=" + interfaceFolder + "/" + interface_name + "_ip")
+            config_file.write("set Interface Ethernet " + interface_name + " IP=" + interfaceFolder + "/" + interface_name + "_ip")
             config_file.write("\n")
 
 config_file.close()
